64aaa5dc1c76052b4c09a96a4ae84a5c01ec1eb9
techienicke <developernick.e@gmail.com>
2024-04-09 22:36:48 +0530

    some fixes and upgrades

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import './Components.css'
+import { Link } from 'react-router-dom'
 
 const Mainpage = () => {
   return (
-    <div className="main_container">
-      <div className="main_first_section w-2/4w">
+    <div className="main_container background_image flex justify-center">
+      <div className="main_first_section text-center">
+        <span className='block text-2xl span_add mb-2'>Handmade and crafts</span>
         <span className='navbar_span_nitya justify-end text-8xl text-white'>Nitya</span>
         <span className='navbar_span_maa text-8xl text-white px-2'>माँ</span>
-        <span className='text-white text-base block px-2'>NH15, Kedarnath road, Almora city, Uttarakhand, India</span>
-      </div>
-      <div className="main_social_info text-white text-2xl w-2/4w mt-5">
-        <i class="fa-brands fa-instagram px-5"></i>
-        <i class="fa-brands fa-facebook px-5"></i>
-        <i class="fa-brands fa-x-twitter px-5"></i>
+        <span className='text-base block px-2 span_add font-semibold'>NH15, Kedarnath road, Almora city, Uttarakhand, India</span>
+        <div className="main_social_info text-white text-2xl w-2/4w mt-5 flex justify-center">
+          <Link to='/'><i class="fa-brands fa-instagram px-5 cursor-pointer"></i></Link>
+          <Link to='/'><i class="fa-brands fa-facebook px-5 cursor-pointer"></i></Link>
+          <Link to='/'><i class="fa-brands fa-x-twitter px-5 cursor-pointer"></i></Link>
+        </div>
       </div>
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,24 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <div className='navbar_container bg-gray-800 text-white grid sm:grid-cols-12 gap-3 items-center'>
+        <div className='navbar_container background_image text-white grid sm:grid-cols-12 gap-3 items-center'>
             <div className="navbar_left grid justify-center sm:col-span-2 items-center min-h-[9vh]">
                 <Link to='/'>
                     <span className='navbar_span_nitya justify-end text-2xl'>Nitya</span>
                     <span className='navbar_span_maa text-2xl'>माँ</span>
                 </Link>
             </div>
-            <div className="navbar_center sm:col-span-10 min-h-[9vh] items-center grid justify-end">
+            <div className="navbar_center sm:col-span-8 min-h-[9vh] items-center grid justify-center">
                 <ul className='navbar_ul flex'>
-                    <Link to="/menu"><li>Menu</li></Link>
-                    <Link to="/contact"><li>Contact</li></Link>
-                    <Link to="/booktable"><li>Book a table</li></Link>
+                    <Link to="/menu"><li className='p-5'>Menu</li></Link>
+                    <Link to="/contact"><li className='p-5'>Contact</li></Link>
+                    <Link to="/booktable"><li className='p-5'>Blog</li></Link>
                 </ul>
+            </div>
+            <div className="navbar_right sm:col-span-2 min-h-[9vh] flex items-center justify-center">
+                <Link to='/'><i class="fa-solid fa-magnifying-glass p-4 span_add"></i></Link>
+                <Link to='/'><i class="fa-regular fa-user p-4 span_add"></i></Link>
+                <Link to='/'><i class="fa-solid fa-bag-shopping p-4 span_add"></i></Link>
             </div>
         </div>
     )
