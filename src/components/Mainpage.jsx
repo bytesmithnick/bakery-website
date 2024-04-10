8bb349fd3aec5b9ee80ba494fb577b15518e41dd
techienicke <developernick.e@gmail.com>
2024-04-10 09:37:13 +0530

    ui changes

--- src/components/Mainpage.jsx
+++ src/components/Mainpage.jsx
@@ -6,10 +6,9 @@ const Mainpage = () => {
   return (
     <div className="main_container background_image flex justify-center">
       <div className="main_first_section text-center">
-        <span className='block text-2xl span_add mb-2'>Handmade and crafts</span>
-        <span className='navbar_span_nitya justify-end text-8xl text-white'>Nitya</span>
-        <span className='navbar_span_maa text-8xl text-white px-2'>माँ</span>
-        <span className='text-base block px-2 span_add font-semibold'>NH15, Kedarnath road, Almora city, Uttarakhand, India</span>
+        <span className='block text-2xl span_add mb-2 font_lobster'>Handmade and crafts</span>
+        <span className='font_dancing justify-end text-8xl text-white'>Nitya  Maa</span>
+        <span className='text-lg block px-2 span_add font-semibold font_josefin mt-5'>NH15, Kedarnath road, Almora city, Uttarakhand, India</span>
         <div className="main_social_info text-white text-2xl w-2/4w mt-5 flex justify-center">
           <Link to='/'><i class="fa-brands fa-instagram px-5 cursor-pointer"></i></Link>
           <Link to='/'><i class="fa-brands fa-facebook px-5 cursor-pointer"></i></Link>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ const Navbar = () => {
     return (
         <div className='navbar_container background_image text-white grid sm:grid-cols-12 gap-3 items-center'>
             <div className="navbar_left grid justify-center sm:col-span-2 items-center min-h-[9vh]">
-                <Link to='/'>
-                    <span className='navbar_span_nitya justify-end text-2xl'>Nitya</span>
+                <Link to='/' className='nav_home_link'>
+                    <span className='font_dancing justify-end text-2xl'>Nitya</span>
                     <span className='navbar_span_maa text-2xl'>माँ</span>
                 </Link>
             </div>
             <div className="navbar_center sm:col-span-8 min-h-[9vh] items-center grid justify-center">
                 <ul className='navbar_ul flex'>
-                    <Link to="/menu"><li className='p-5'>Menu</li></Link>
-                    <Link to="/contact"><li className='p-5'>Contact</li></Link>
-                    <Link to="/booktable"><li className='p-5'>Blog</li></Link>
+                    <Link to="/menu"><li>Menu</li></Link>
+                    <Link to="/contact"><li >Contact</li></Link>
+                    <Link to="/booktable"><li >Blog</li></Link>
                 </ul>
             </div>
             <div className="navbar_right sm:col-span-2 min-h-[9vh] flex items-center justify-center">
